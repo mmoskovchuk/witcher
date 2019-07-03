026a823e89a3632d995f30c2ca2961c3541514ec
viajoker <max.moskovchuk@gmail.com>
2019-07-03 23:56:23 +0300

    edited responsive

--- js/custom.js
+++ js/custom.js
@@ -25,21 +25,3 @@ document.addEventListener('DOMContentLoaded', function () {
         document.execCommand("copy");
     });
 });
-
-
-$(document).ready(function() {
-    $('.paraWrapper').mousemove(function(e) {
-        parallax(e, document.getElementById('c8'), 1);
-    });
-});
-
-function parallax(e, target, layer) {
-    var strength = 20;
-    var layer_coeff = strength / layer;
-    var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
-    var y = ($(window).height() - target.offsetHeight) / 2 - (e.pageY - ($(window).height() / 2)) / layer_coeff;
-    $(target).offset({
-        top: y,
-        left: x
-    });
-};
